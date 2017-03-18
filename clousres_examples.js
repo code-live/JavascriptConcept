8ac37a05ae1b7b97330be375960e73e699de2528
virendra <vpsk05@gmail.com>
2017-03-19 03:23:07 +0530

    clousre examples added

--- clousres_examples.js
+++ clousres_examples.js
@@ -15,4 +15,26 @@ var number = numberGenerator();
 														  console.log(a + num);
 														}
 */
-number(1); // 2   
+number(1); // 3  
+
+--------------------------------------------------------------------------------------------------
+
+function sayHello() {
+	hello = 'Hello, world!';
+  var say = function() {
+   console.log(hello);
+  }
+  // Local variable that ends up within the closure 
+  var hello;
+  return say;
+}
+var sayHelloClosure = sayHello(); 
+
+/*
+	in return number will grt this function body 		function checkNumber(a) { 
+														  console.log(a + num);
+														}
+*/
+sayHelloClosure();
+
+--------------------------------------------------------------------------------------------------
